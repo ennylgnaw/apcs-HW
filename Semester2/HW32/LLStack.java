0858c38d954fcc641dec1c021ac13be141610f77
ennylgnaw <lwang7@stuy.edu>
2018-04-12 04:54:42 -0400

    Added HW32

--- Semester2/HW32/LLStack.java
+++ Semester2/HW32/LLStack.java
@@ -3,28 +3,23 @@
 //HW #32: Leon Leonwood Stack
 //2018-04-12
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class ALStack<T> implements Stack<T> {
+public class LLStack<T> implements Stack<T> {
 
     //instance vars
-    private ArrayList<T> _stack;
+    private LinkedList<T> _stack;
 
     //constructor
-    public ALStack() {
-	_stack = new ArrayList<T>();
-    }
-
-    //overloaded constructor
-    public ALStack( int n ) {
-        _stack = new ArrayList<T>(n);
+    public LLStack() {
+	_stack = new LinkedList<T>();
     }
 
     //Methods
 
     //Return true if this stack is empty, otherwise false.
     public boolean isEmpty() {
-	return (_stack.size() == 0);
+	return (_stack.size() == 0); 
     }
 
     //Return top element of stack without popping it.
@@ -41,9 +36,8 @@ public class ALStack<T> implements Stack<T> {
 	return _stack.remove(_stack.size() - 1);
     }
 
-    //Push an element onto top of this stack.
     public void push( T x ) {
-	_stack.add(x);
+	_stack.add( x );
     }
     
 }
--- Semester2/HW32/Stckr.java
+++ Semester2/HW32/Stckr.java
@@ -1,16 +1,43 @@
 /*****************************************************
  * class Stckr
-* driver/tester for Stack implementations (Linked-list-based, ArrayList-based)
+ * driver/tester for Stack implementations (Linked-list-based, ArrayList-based)
  *****************************************************/
 
 public class Stckr 
 {
-public static void main( String[] args )
-{
-//Stack cakes = new ALStack()...
-//Stack cakes = new LLStack()...
+    public static void main( String[] args )
+    {   
+	//LLStack
+	System.out.println("______LLStack______");
+	Stack cakes = new LLStack<String>();
+	cakes.push("Tuvan");
+	cakes.push("throat");
+	cakes.push("singing");
+	System.out.println(cakes.peek()); //singing
+	System.out.println(cakes.pop()); //singing
+	System.out.println(cakes.pop()); //throat
 
-}
+	//empty LLStack
+	Stack sadCakes = new LLStack<String>();
+	System.out.println(sadCakes.peek()); //null
+	System.out.println(sadCakes.pop()); //null
+    
+
+	//ALStack
+	System.out.println("______ALStack______");
+	Stack latkes = new ALStack<String>();
+	latkes.push("Tuvan");
+	latkes.push("throat");
+	latkes.push("singing");
+	System.out.println(latkes.peek()); //singing
+	System.out.println(latkes.pop()); //singing
+	System.out.println(latkes.pop()); //throat
+
+	//empty ALStack
+	Stack sadLatkes = new ALStack<String>();
+	System.out.println(sadLatkes.peek()); //null
+	System.out.println(sadLatkes.pop()); //null
+    }
 
 }//end class
 
