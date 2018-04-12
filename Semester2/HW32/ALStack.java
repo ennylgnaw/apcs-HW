a208e196c7bc2467d4bcbd1507e085cb7df6e1fa
ennylgnaw <lwang7@stuy.edu>
2018-04-12 03:24:43 -0400

    Added HW34

---
+++ Semester2/HW32/ALStack.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ALStack implements Stack {
+
+    //instance vars
+    ArrayList arr;
+    
+    public booloean isEmpty() {
+	return (arr.size() == 0);
+    }
+    
+}
